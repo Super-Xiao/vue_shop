44871f22e513995debcef599486af1f6cb35c6e0
Super-xiao <1446751909@qq.com>
2021-11-11 16:15:16 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './plugins/element.js'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/css/global.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.prototype.$http = axios
+axios.defaults.baseURL = "https://lianghj.top:8888/api/private/v1/"
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img
+          src="https://img1.baidu.com/it/u=3256750006,1716423677&fm=26&fmt=auto"
+          alt=""
+        />
+      </div>
+      <!-- 登陆表单区域 -->
+      <el-form
+        label-width="0px"
+        class="login_form"
+        :model="loginForm"
+        :rules="loginRules"
+        ref="loginFormRef"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username"
+            ><i slot="prefix" class="el-input__icon el-icon-user"></i
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" type="password"
+            ><i slot="prefix" class="el-input__icon el-icon-lock"></i
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="loginClick">登录</el-button>
+          <el-button type="info" @click="resetClick">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 登录表单的数据绑定对象
+      loginForm: {
+        username: "admin",
+        password: "123456",
+      },
+      loginRules: {
+        username: [
+          { required: true, message: "请输入登录账号", trigger: "blur" },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          { required: true, message: "请输入登陆密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    resetClick() {
+      //   console.log(1);
+      this.$refs.loginFormRef.resetFields();
+    },
+    loginClick() {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (!valid) return;
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        if (res.meta.status !== 200)
+          this.$message({
+            message: "登录出了一点问题",
+            type: "warning",
+          });
+         this.$message.success('登陆成功');
+         console.log(res);
+
+        //  保存token
+        window.sessionStorage.setItem('token',res.data.token)
+        this.$router.push('/home')
+      });
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #362e49;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      height: 100%;
+      width: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: end;
+}
+</style>
